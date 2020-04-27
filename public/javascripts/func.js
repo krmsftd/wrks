eb93d472b377d8e7741ad33c159c33cd85c624c1
impregnable <ImpregnableFiend@gmail.com>
2020-04-28 00:11:05 +0300

    Code is adjusted.

--- public/javascripts/func.js
+++ public/javascripts/func.js
@@ -1,6 +1,15 @@
-setTimeout( () => {
-    const input = document.getElementById('input');
-    const buttonRmw = document.getElementById('buttonRmv');
-    const inputAdd = document.getElementById('inputAdd');
-    const buttonAdd = document.getElementById('buttonAdd');
-     })
+/**
+ * Оборачиваем весь контент в функцию, которую потом передаём в качестве аргумента для listener'а.
+ * Делаем это для того чтобы обращаться к элементам после того как они точно созданы в DOM.
+ */
+const onDOMLoaded = () => {
+    /**
+     * Пытайся называть переменные как-то понятнее и в целом разберись какие тебе нужны, а какие нет.
+     */
+    const REMOVE_INPUT = document.getElementById('inputRemove');
+    const REMOVE_BTN = document.getElementById('sendRemove');
+    const ADD_INPUT = document.getElementById('inputAdd');
+    const ADD_BTN = document.getElementById('sendAdd');
+};
+
+document.addEventListener('DOMContentLoaded', onDOMLoaded);
