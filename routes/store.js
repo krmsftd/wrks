516d7225ed006b6bc77011ca6c237ed26094b2d5
krmsftd <skmail100@gmail.com>
2020-05-01 22:34:23 +0500

    add opportunity write in file in func router.post

--- routes/store.js
+++ routes/store.js
@@ -1,19 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const fs = require('fs');
+const FILE = '\\wrks\\data\\store.txt';
 
 router.use(timeLog = (req, res, next) => {
   console.log('Current date: ', new Date().toJSON().slice(0, 10));
   next();
 });
 
-
 /**
  * Handle POST-request here.
  */
   router.post('/', (req, res) => {
-    console.log('req=>',req.body);
-    res.send({responseLAR: "file was saved!"});
-
-    console.log("ZAVELOS!");
-  })
+    fs.writeFile(FILE,JSON.stringify(req.body), (err) => {
+      if (err) {
+        return console.log(err);
+      }
+      res.send({responseLAR: "saved!"});
+    } );
+  });
 module.exports = router;
