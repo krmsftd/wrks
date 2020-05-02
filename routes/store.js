3d7f68945b0f1c2f4b3eac5a57bf630428d4c205
impregnable <ImpregnableFiend@gmail.com>
2020-05-02 19:04:07 +0300

    [B] Store router is adjusted.

--- routes/store.js
+++ routes/store.js
@@ -1,22 +1,21 @@
 const express = require('express');
 const router = express.Router();
 const fs = require('fs');
-const FILE = '\\wrks\\data\\store.txt';
+
+const PATH = 'store.txt';
 
 router.use(timeLog = (req, res, next) => {
-  console.log('Current date: ', new Date().toJSON().slice(0, 10));
-  next();
+    console.log('Current date: ', new Date().toJSON().slice(0, 10));
+    next();
 });
 
-/**
- * Handle POST-request here.
- */
-  router.post('/', (req, res) => {
-    fs.writeFile(FILE,JSON.stringify(req.body), (err) => {
-      if (err) {
-        return console.log(err);
-      }
-      res.send({responseLAR: "saved!"});
-    } );
-  });
+router.post('/', (req, res) => {
+    fs.writeFile(PATH, JSON.stringify(req.body), (err) => {
+        if (err) {
+            return console.error(err);
+        }
+        res.send({ ok: true });
+    });
+});
+
 module.exports = router;
